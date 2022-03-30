b663c613b7cc94caa9235e9de8278c3bdc44df18
Jamie Samuel <jsam07@hotmail.com>
2022-03-29 18:45:55 -0700

    Add db, models, and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SnippitController;
+use App\Http\Controllers\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/snippit', 'SnippitController@index');
+Route::get('/tag', 'TagController@index');
